4e2c86ca2c62962a6b18c73c1dea3d32d0896087
fadliachsan27 <fadliachsan010@gmail.com>
2025-03-12 08:06:51 +0700

    menambahkan class FanLaptop

---
+++ jurnalModul4_103022300018/FanLaptop.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace jurnalModul4_103022300018
+{
+    class FanLaptop
+    {
+
+        public enum State {Queit, Balanced, Perfomance, Turbo}
+        
+        public enum Trigger {mode_down, mode_up, turbo_shortcut}
+
+        struct transition
+        {
+            public State prevState;
+            public State nextState;
+            public Trigger trigger;
+
+            public transition(State prevState, State nextState, Trigger trigger)
+            {
+                this.prevState = prevState;
+                this.nextState = nextState;
+                this.trigger = trigger;
+            }
+        }
+
+        transition[] transitions =
+            {
+                new transition(State.Queit, State.Balanced, Trigger.mode_up),
+                new transition(State.Queit, State.Turbo, Trigger.turbo_shortcut),
+                new transition(State.Balanced, State.Perfomance, Trigger.mode_up),
+                new transition(State.Balanced, State.Queit, Trigger.mode_down),
+                new transition(State.Perfomance, State.Turbo, Trigger.mode_up),
+                new transition(State.Perfomance, State.Balanced, Trigger.mode_down),
+                new transition(State.Turbo, State.Queit, Trigger.turbo_shortcut),
+                new transition(State.Turbo, State.Perfomance, Trigger.mode_down),
+            };
+
+        public State currentState = State.Queit;
+
+        public State GetState(State prevState, Trigger trigger)
+        {
+            State nextState = State.Queit;
+            foreach (transition t in transitions)
+            {
+                if (t.prevState == prevState && t.trigger == trigger)
+                {
+                    nextState = t.nextState;
+                }
+            }
+        }
+        public void changeState(Trigger trigger)
+        {
+            State nextState = GetState(currentState, trigger);
+            currentState = nextState;
+        }
+    }
+}
---
+++ jurnalModul4_103022300018/kodeProduk.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnalModul4_103022300018
+{
+    class kodeProduk
+    {
+        private Dictionary<string, string> daftarKodeElektronik;
+            public kodeProduk()
+            {
+            daftarKodeElektronik = new Dictionary<string, string>
+            {
+                {"Laptop", "E100"},
+                {"Smartphone", "E101"},
+                {"Tablet", "E102"},
+                {"Headset", "E103"},
+                {"Keyboard", "E104"},
+                {"Mouse", "E105"},
+                {"Printer", "E106"},
+                {"Monitor", "E107"},
+                {"Smartwatch", "E108"},
+                {"Kamera", "E109"}
+            };
+            }
+
+        public string GetKodeElektronik(String elektronik)
+        {
+            if (daftarKodeElektronik.ContainsKey(elektronik))
+            {
+                return daftarKodeElektronik[elektronik];
+            }
+            else
+            {
+                return "Kode Pos Tidak Ditemukan";
+            }
+        }
+
+        public void TampilkanSemua()
+        {
+            Console.WriteLine("Daftar kode Pos :");
+            foreach (var entry in daftarKodeElektronik)
+            {
+            Console.WriteLine($"{entry.Key}: {entry.Value}");
+            }
+        }
+    }
+}
